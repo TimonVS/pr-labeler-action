3b333927d7b8a04c7c4b246fb4ef42a0fd1ab16a
Timon van Spronsen <timonvanspronsen@outlook.com>
2019-04-06 14:36:51 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const { Toolkit } = require('actions-toolkit')
+const getConfig = require('./utils/config')
+const wildcard = require('wildcard')
+
+const CONFIG_FILENAME = 'config.yml'
+const defaults = {
+  patterns: [
+    { branch: 'feature/*', label: 'feature' },
+    { branch: 'fix/*', label: 'fix' },
+    { branch: 'chore/*', label: 'chore' }
+  ]
+}
+
+Toolkit.run(
+  async tools => {
+    const repoInfo = {
+      owner: tools.context.payload.repository.owner.login,
+      repo: tools.context.payload.repository.name
+    }
+
+    const config = Object.assign(
+      defaults,
+      await getConfig(tools.github, CONFIG_FILENAME, repoInfo)
+    )
+
+    const labelsToAdd = config.patterns.reduce((labels, pattern) => {
+      if (wildcard(pattern.branch, tools.context.ref)) {
+        labels = labels.concat(pattern.label)
+      }
+      return labels
+    }, [])
+
+    if (labelsToAdd.length > 0) {
+      await tools.github.issues.addLabels({
+        number: tools.context.payload.pull_request.number,
+        labels: labelsToAdd,
+        ...repoInfo
+      })
+    }
+
+    tools.exit.success()
+  },
+  { event: 'pull_requests.created' }
+)
---
+++ utils/config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const yaml = require('js-yaml')
+
+const CONFIG_PATH = '.github'
+
+module.exports = async function getConfig(github, fileName, { owner, repo }) {
+  try {
+    const response = await github.repos.getContents({
+      owner,
+      repo,
+      path: path.posix.join(CONFIG_PATH, fileName)
+    })
+
+    return parseConfig(response.data.content)
+  } catch (error) {
+    if (error.code === 404) {
+      return null
+    }
+
+    throw error
+  }
+}
+
+function parseConfig(content) {
+  return yaml.safeLoad(Buffer.from(content, 'base64').toString()) || {}
+}
