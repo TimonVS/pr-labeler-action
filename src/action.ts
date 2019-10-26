c850b2d8d5a823f4308d1d19c1a3c641b0a562df
Timon van Spronsen <timonvanspronsen@outlook.com>
2019-10-26 18:34:41 +0200

    Fix @actions/core import

--- src/action.ts
+++ src/action.ts
@@ -1,4 +1,4 @@
-import core from '@actions/core'
+import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { Context } from '@actions/github/lib/context'
 import matcher from 'matcher'
