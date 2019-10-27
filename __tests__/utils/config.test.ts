ff9b1f1472d5d01e0d5ff929306227f38956fc60
Timon van Spronsen <timonvanspronsen@outlook.com>
2019-10-27 16:36:36 +0100

    Convert config test to TS

---
+++ __tests__/utils/config.test.ts
@@ -0,0 +1,31 @@
+import getConfig from '../../src/utils/config'
+
+describe('getConfig', () => {
+  it('returns default config when GitHub returns a 404 for given path', async () => {
+    const defaultConfig = {
+      foo: 'bar'
+    }
+
+    const githubMock = {
+      repos: {
+        getContents() {
+          throw new HTTPError(404)
+        }
+      }
+    }
+
+    const config = await getConfig(
+      githubMock as any,
+      'path/to/config',
+      { owner: 'repo-owner', repo: 'repo-name' },
+      'ref',
+      defaultConfig
+    )
+
+    expect(config).toBe(defaultConfig)
+  })
+})
+
+class HTTPError {
+  constructor(public status: number) {}
+}
